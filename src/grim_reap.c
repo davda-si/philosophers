55579c7d91d11a4e43512e3c23cd7471842d62f1
david <davda-si@student.42lisboa.com>
2024-01-30 02:35:10 +0000

    add sum minor stuff

--- src/grim_reap.c
+++ src/grim_reap.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 17:25:11 by david             #+#    #+#             */
-/*   Updated: 2024/01/29 18:51:55 by david            ###   ########.fr       */
+/*   Updated: 2024/01/29 21:08:36 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	philo_ate(t_table *ph, t_philo *marx)
 	pthread_mutex_lock(&(marx->eatin));
 	if (marx->ate >= ph->meals)
 		marx->last_meal = 1;
-	if (timer() >= ph->tm_die)
+	if (timer() >= marx->time_meal)
 		ph->dead = 1;
 	pthread_mutex_unlock(&(marx->eatin));
 	return (ph->dead);
